af728ecab3bb7445e3eab9afe86fcae3f4683416
Rithik Garimella <rgarim@umich.edu>
2023-05-24 19:48:19 -0400

    login and create account page styling

--- container/container/views.py
+++ container/container/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.db import connections
 from django.http import HttpResponse
+from datetime import datetime
 
-def home(request):
-    return render(request, 'container/index.html', {"logged_in": request.session.get('is_logged_in'), "reload": False})
+
+def home(request): 
+    
+    current_time = datetime.now().hour # 
+    
+    greeting = ""
+    
+    if current_time >= 6 and current_time < 12: 
+        greeting = "Good Morning"
+    elif current_time >= 12 and current_time < 18: 
+        greeting = "Good Afternoon"
+    else: 
+        greeting = "Good Evening"
+        
+    return render(request, 'container/index.html', {"logged_in": request.session.get('is_logged_in'), "greeting" : greeting, "name": request.session.get('name')})
--- container/log/views.py
+++ container/log/views.py
@@ -2,65 +2,78 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from . models import User
 from store.models import Shopper
+from django.shortcuts import redirect
+
 
 def login(request):
-    if request.method == 'POST': 
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
-        if request.session.get('is_logged_in'): 
-            return render(request, 'log/login.html', {"state" : "AlreadyLoggedIn", "logged_in": request.session.get('is_logged_in')})
-         
+
+        if request.session.get('is_logged_in'):
+            return render(request, 'log/login.html', {"state": "AlreadyLoggedIn", "logged_in": request.session.get('is_logged_in')})
+
         my_user = User.objects.filter(username=username)
 
-        if len(my_user) == 0: 
-            return render(request, 'log/login.html', {"state" : "NoUser", "logged_in": request.session.get('is_logged_in')})
-        elif len(my_user) == 1 and my_user[0].password == password: 
+        if len(my_user) == 0:
+            return render(request, 'log/login.html', {"state": "NoUser", "logged_in": request.session.get('is_logged_in')})
+        elif len(my_user) == 1 and my_user[0].password == password:
             request.session['is_logged_in'] = True
             request.session['username'] = my_user[0].username
             request.session['name'] = my_user[0].first_name
-            return render(request, 'log/login.html', {"state" : "Success", "logged_in": request.session.get('is_logged_in')})
-        elif my_user[0].password != password: 
-            return render(request, 'log/login.html', {"state" : "WrongPassword", "logged_in": request.session.get('is_logged_in')})
-        else: 
-            return render(request, 'log/login.html', {"state" : "Error", "logged_in": request.session.get('is_logged_in')})
-    return render(request, 'log/login.html', {"state" : "", "logged_in": request.session.get('is_logged_in')})
-   
-    
+            return redirect('home')
+        elif my_user[0].password != password:
+            return render(request, 'log/login.html', {"state": "WrongPassword", "logged_in": request.session.get('is_logged_in')})
+        else:
+            return render(request, 'log/login.html', {"state": "Error", "logged_in": request.session.get('is_logged_in')})
+    return render(request, 'log/login.html', {"state": "", "logged_in": request.session.get('is_logged_in')})
 
+
+def auto_login(request, username, password):
+    my_user = User.objects.filter(username=username)
+    request.session['is_logged_in'] = True
+    request.session['username'] = my_user[0].username
+    request.session['name'] = my_user[0].first_name
+    
+    return redirect('home')
+   
 
 def logout(request):
-    
-    if request.session.get('is_logged_in'): 
+
+    if request.session.get('is_logged_in'):
         request.session['is_logged_in'] = False
         request.session['username'] = ""
         request.session['name'] = ""
-        print("went in here!")
-        return render(request, 'container/index.html', {"reload": "True"})
+        return redirect('/log/login')
     else:
-        return render(request, 'log/logout.html', {"state" : "NotLoggedIn"})
-        
-        
+        return render(request, 'log/login.html', {"state": "NotLoggedIn"})
 
 
-def newUser(request): 
-    if request.method == 'POST': 
+def newUser(request):
+   
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        re_password = request.POST['re-password']
         email = request.POST['email']
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        
+
+        if password != re_password:
+            return render(request, 'log/createUser.html', {"state": "PasswordMismatch", "logged_in": request.session.get('is_logged_in')})
         my_user = User.objects.filter(username=username)
-        
-        if len(my_user) == 0: 
-            new_user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        my_user_email = User.objects.filter(email=email)
+        if len(my_user) == 0 and len(my_user_email) == 0:
+            new_user = User(username=username, password=password,
+                            email=email, first_name=first_name, last_name=last_name)
             new_user.save()
             add_shopper = Shopper(username=username, name=first_name, items="")
             add_shopper.save()
-            
-            return render(request, 'log/createUser.html', {"state" : "Success"})
-        else: 
-            return render(request, 'log/createUser.html', {"state" : "UserExists"})
-    return render(request, 'log/createUser.html', {"state" : ""})
-   
+
+            return auto_login(request, username, password)
+        else:
+            if len(my_user) > 0:
+                return render(request, 'log/createUser.html', {"state": "UserExists", "logged_in": request.session.get('is_logged_in')})
+            elif len(my_user_email) > 0:
+                return render(request, 'log/createUser.html', {"state": "EmailExists", "logged_in": request.session.get('is_logged_in')})
+    return render(request, 'log/createUser.html', {"state": "", "logged_in": request.session.get('is_logged_in')})
